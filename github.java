cd2380217f399c8c962d5ee46862b615365072dd
Aaron Hsu <pufflewizzy@gmail.com>
2019-11-03 21:04:28 -0500

    hello

--- github.java
+++ github.java
@@ -1,8 +1,10 @@
 public class github {
   public static void main(String[] args) {
-    System.out.println("This is a test");
-    System.out.println("idk what to do");
-    System.out.println("why");
-    System.out.println("why not");
+    if(true) {
+      System.out.println("hello");
+    }
+    if(false) {
+      System.out.println("bye");
+    }
   }
 }
