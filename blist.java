500115c9d79ea3b48f2e38bafae694a683e7d4df
Aaron Hsu <pufflewizzy@gmail.com>
2019-11-13 14:15:07 -0400

    easf

---
+++ blist.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class blist {
+  public static void main(String[] args) throws FileNotFoundException{
+    Scanner input = new Scanner(new File("blist.in"));
+    PrintWriter output = new PrintWriter(new File("blist.out"));
+
+    // Start code here
+    int cows = input.nextInt();
+    int[][] milked = new int[cows][3];
+
+    for(int i = 0; i < cows; i++) {
+      for(int a = 0; a < 3; a++) {
+        milked[i][a] = input.nextInt();
+      }
+    }
+
+    int[] time = new int[cows];
+    for(int i = 0; i < cows; i++) {
+      time[i] = milked[i][1];
+    }
+
+    Arrays.sort(time);
+
+    int[] buckets = new int[time[time.length - 1]];
+
+    for(int i = 0; i < cows; i++) {
+      for(int a = 0; a < buckets.length; a++) {
+        if(a >= milked[i][0] && a <= milked[i][1]) {
+          buckets[a] += milked[i][2];
+        }
+      }
+      System.out.println(Arrays.toString(buckets));
+    }
+
+    Arrays.sort(buckets);
+
+    output.write(Integer.toString(buckets[buckets.length - 1]));
+    output.close();
+  }
+}
---
+++ lifeguards.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+// COMPLETE 10/10
+
+public class lifeguards {
+  public static void main(String[] args) throws FileNotFoundException{
+    Scanner input = new Scanner(new File("lifeguards.in"));
+    PrintWriter output = new PrintWriter(new File("lifeguards.out"));
+
+    // Start code here
+
+    int numCows = input.nextInt();
+
+    int[][] lifeguard = new int[numCows][2];
+
+    for(int i = 0; i < numCows; i++) {
+      lifeguard[i][0] = input.nextInt();
+      lifeguard[i][1] = input.nextInt();
+    }
+
+    int[] shifts = new int[1000];
+
+    for(int i = 0; i < numCows; i++) {
+      for(int a = lifeguard[i][0]; a < lifeguard[i][1]; a++) {
+        shifts[a - 1]++;
+      }
+    }
+
+    int[] temp = shifts.clone();
+    int[] remainingShifts = new int[numCows];
+    int sum = 0;
+
+    for(int a = 0; a < numCows; a++) {
+      for(int i = lifeguard[a][0]; i < lifeguard[a][1]; i++) {
+        temp[i - 1]--;
+      }
+
+      for(int b = 0; b < temp.length; b++) {
+        if(temp[b] > 0) {
+          sum++;
+        }
+      }
+
+      remainingShifts[a] = sum;
+      sum = 0;
+      temp = shifts.clone();
+
+    }
+
+    Arrays.sort(remainingShifts);
+    int highest = remainingShifts[remainingShifts.length - 1];
+
+    output.write(Integer.toString(highest));
+    output.close();
+
+  }
+}
