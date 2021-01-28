56b8a9453b69b8962c2f5def64f0e1cc664a1ced
Aaron Hsu <pufflewizzy@gmail.com>
2021-01-28 11:33:59 -0500

    setup for gold

---
+++ Gold/Gold v1/_Template.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class _Template {
+	static StreamTokenizer in;
+	
+	static int nextInt() throws Exception {
+		in.nextToken();
+		return (int) in.nval;
+	}
+
+	static String next() throws Exception {
+		in.nextToken();
+		return (String) in.sval;
+	}
+	
+	static long nextLong() throws Exception {
+		in.nextToken();
+		return (long) in.nval;
+	}
+
+	public static void main(String[] args) throws Exception {
+		in = new StreamTokenizer(new BufferedReader(new FileReader("_Template.in")));
+
+		
+	}
+}
